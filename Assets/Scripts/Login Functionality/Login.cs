01ea4ff2317687fc628d86b2fd03b75e1a922e5f
DevEpi <blockproductions123@gmail.com>
2023-11-22 17:43:52 -0800

    login/register
    fixed variable name errors
    fixed setup of logins.txt

--- Assets/Scripts/Login Functionality/Login.cs
+++ Assets/Scripts/Login Functionality/Login.cs
@@ -8,43 +8,71 @@ using UnityEngine.SceneManagement;
 
 public class Login : MonoBehaviour
 {
+
     public InputField usernameInput;
     public InputField passwordInput;
     public Button loginButton;
     public Button goToRegisterButton;
 
-    private List<string> credentials; // Use List<T> instead of ArrayList
+    ArrayList log;
 
-    private void Start()
+    // Start is called before the first frame update
+    void Start()
     {
-        loginButton.onClick.AddListener(LoginUser);
-        goToRegisterButton.onClick.AddListener(MoveToRegister);
+        loginButton.onClick.AddListener(login);
+        goToRegisterButton.onClick.AddListener(registerScene);
 
-        string filePath = Application.dataPath + "/logins.txt";
-        if (File.Exists(filePath))
+        if (File.Exists(Application.dataPath + "/logins.txt"))
         {
-            credentials = new List<string>(File.ReadAllLines(filePath));
+            log = new ArrayList(File.ReadAllLines(Application.dataPath + "/logins.txt"));
         }
         else
         {
-            Debug.Log("Logins file doesn't exist");
+            Debug.Log("logins file doesn't exist");
         }
+
     }
 
-    private void LoginUser()
+
+
+    // Update is called once per frame
+    void login()
     {
         bool isExists = false;
 
-        credentials = new List<string>(File.ReadAllLines(Application.dataPath + "/logins.txt"));
+        log = new ArrayList(File.ReadAllLines(Application.dataPath + "/logins.txt"));
 
-        foreach (string line in credentials)
+        foreach (var i in log)
         {
-            //everything after
-            string[] parts = line.Split(':');
-            if (parts.Length == 2 && parts[0].Equals(usernameInput.text) && parts[1].Equals(passwordInput.text))
+            string line = i.ToString();
+            
+            //substring 0-indexof(:) - indexof(:)+1 - i.length-1
+            if (i.ToString().Substring(0, i.ToString().IndexOf(":")).Equals(usernameInput.text) &&
+                i.ToString().Substring(i.ToString().IndexOf(":") + 1).Equals(passwordInput.text))
             {
                 isExists = true;
                 break;
             }
         }
+
+        if (isExists)
+        {
+            Debug.Log($"Logging in '{usernameInput.text}'");
+            welcomeScene();
+        }
+        else
+        {
+            Debug.Log("Incorrect login");
+        }
     }
+
+    void registerScene()
+    {
+        SceneManager.LoadScene("Register");
+    }
+
+    void welcomeScene()
+    {
+        SceneManager.LoadScene("WelcomeScreen");
+    }
+}
