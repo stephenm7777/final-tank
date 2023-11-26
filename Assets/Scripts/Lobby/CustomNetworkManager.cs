382c3c5c1132c74045967285cb70b4b1b952da24
Stephen Martinez <122754065+stephenm7777@users.noreply.github.com>
2023-11-25 23:44:39 -0800

    Lobby
    Fixing the lobby for multiplayer

--- Assets/Scripts/Lobby/CustomNetworkManager.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Mirror;
-public class CustomNetworkManager : NetworkManager
-{
-    // Start is called before the first frame update
-}
