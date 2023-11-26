1b7c626906b969347f934efac7b9aa929db40d56
Stephen Martinez <122754065+stephenm7777@users.noreply.github.com>
2023-11-26 01:36:42 -0800

    Lobby UI and Server UI

---
+++ Assets/Scripts/ConnectToServer/ConnectToServer.cs
@@ -0,0 +1,20 @@
+using Photon.Pun; 
+using UnityEngine.UI; 
+using UnityEngine.SceneManagement;
+
+public class ConnectToServer : MonoBehaviourPunCallbacks
+{
+    public InputField usernameInput; 
+    public Text buttonText; 
+    public void OnClickConnect(){
+        if(usernameInput.text.Length >= 1){
+            PhotonNetwork.NickName = usernameInput.text;
+            buttonText.text = "Connecting"; 
+            PhotonNetwork.ConnectUsingSettings(); 
+            
+        }
+    }
+    public override void OnConnectedToMaster(){
+        SceneManager.LoadScene("Lobby");
+    }
+}
