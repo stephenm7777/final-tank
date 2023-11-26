db7142980ca874f5c569b460d2af06d996fffc1f
Stephen Martinez <122754065+stephenm7777@users.noreply.github.com>
2023-11-26 04:08:16 -0800

    Update ConnectToServer.cs

--- Assets/Scripts/Lobby/Lobby.cs
+++ Assets/Scripts/Lobby/Lobby.cs
@@ -1,23 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Photon.Pun; 
-using Photon.Realtime; 
-using UnityEngine.UI; 
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
 
 public class Lobby : MonoBehaviourPunCallbacks
 {
     public InputField roomInputField;
-    public GameObject lobbyPanel; 
-    public GameObject roomPanel; 
-    public Text roomName; 
-    public RoomItem roomItemPrefab; 
-    List<RoomItem> roomItemsList = new List<RoomItem>(); 
-    public Transform contentObject; 
+    public GameObject lobbyPanel;
+    public GameObject roomPanel;
+    public Text roomName;
+    public RoomItem roomItemPrefab;
+    List<RoomItem> roomItemsList = new List<RoomItem>();
+    public Transform contentObject;
 
-        public void Awake()
+    public void Awake()
     {
-        if (PhotonNetwork.NetworkClientState == ClientState.Disconnected)
+        if (!PhotonNetwork.IsConnected)
         {
             PhotonNetwork.ConnectUsingSettings();
         }
@@ -27,35 +27,40 @@ public class Lobby : MonoBehaviourPunCallbacks
     {
         if (roomInputField.text.Length >= 1)
         {
-            PhotonNetwork.CreateRoom(roomInputField.text, new RoomOptions() { MaxPlayers = 3 });
+            PhotonNetwork.CreateRoom(roomInputField.text, new RoomOptions { MaxPlayers = 3, IsVisible = true });
         }
     }
 
     public override void OnJoinedRoom()
     {
-        lobbyPanel.SetActive(false); 
-        roomPanel.SetActive(true); 
-        roomName.text = "Room name: " + PhotonNetwork.CurrentRoom.Name; 
+        lobbyPanel.SetActive(false);
+        roomPanel.SetActive(true);
+        roomName.text = "Room name: " + PhotonNetwork.CurrentRoom.Name;
     }
 
     public override void OnConnectedToMaster()
     {
         PhotonNetwork.JoinLobby();
     }
+
     public override void OnRoomListUpdate(List<RoomInfo> roomList)
     {
         UpdateRoomList(roomList);
     }
-    void UpdateRoomList(List<RoomInfo> list){
-        foreach(RoomItem item in roomItemsList){
+
+    void UpdateRoomList(List<RoomInfo> list)
+    {
+        foreach (RoomItem item in roomItemsList)
+        {
             Destroy(item.gameObject);
         }
         roomItemsList.Clear();
-        
-        foreach(RoomInfo room in list){
-           RoomItem newRoom = Instantiate(roomItemPrefab, contentObject); 
-           newRoom.SetRoomName(room.Name);
-           roomItemsList.Add(newRoom);
+
+        foreach (RoomInfo room in list)
+        {
+            RoomItem newRoom = Instantiate(roomItemPrefab, contentObject);
+            newRoom.SetRoomName(room.Name);
+            roomItemsList.Add(newRoom);
         }
     }
 }
