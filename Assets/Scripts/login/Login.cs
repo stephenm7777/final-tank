6c188b7a69648d78b27df96ba0aa701771e456a9
DevEpi <blockproductions123@gmail.com>
2023-11-30 13:50:13 -0800

    working
    working gui

--- Assets/Scripts/login/Login.cs
+++ Assets/Scripts/login/Login.cs
@@ -10,61 +10,103 @@ using PlayFab.ClientModels;
 
 public class Login : MonoBehaviour
 {
-    [Header("UI")]
     public TMP_Text messageText;
     public TMP_InputField emailInput;
     public TMP_InputField passwordInput;
+    public TMP_InputField usernameInput;
     public Button loginButton;
     public Button registerButton;
+    const string LAST_EMAIL_KEY = "LAST_EMAIL", LAST_PASSWORD_KEY = "LAST_PASSWORD";
 
     void Start()
     {
-        loginButton.onClick.AddListener(LoginUser);
-        registerButton.onClick.AddListener(RegisterUser);
+        loginButton.onClick.AddListener(LoginPressed);
+        registerButton.onClick.AddListener(RegisterPressed);
     }
 
-    void LoginUser()
+    void LoginPressed()
     {
-        var request = new LoginWithEmailAddressRequest
-        {
-            Email = emailInput.text,
-            Password = passwordInput.text
-        };
-        PlayFabClientAPI.LoginWithEmailAddress(request, OnLoginSuccess, OnError);
+        LoginUser(emailInput.text, passwordInput.text);
     }
 
-  
-    void RegisterUser()
+    void RegisterPressed()
+    {
+        RegisterUser(emailInput.text, usernameInput.text, passwordInput.text);
+    }
+
+    void LoginUser(string email, string password)
+    {
+        PlayFabClientAPI.LoginWithEmailAddress(new LoginWithEmailAddressRequest()
+        {
+            Email = email,
+            Password = password,
+            InfoRequestParameters = new GetPlayerCombinedInfoRequestParams()
+            {
+                GetPlayerProfile = true
+            }
+        },
+        result =>
+        {
+            if (result != null && result.InfoResultPayload != null && result.InfoResultPayload.PlayerProfile != null)
+            {
+                PlayerPrefs.SetString(LAST_EMAIL_KEY, email);
+                PlayerPrefs.SetString(LAST_PASSWORD_KEY, password);
+
+                string displayName = result.InfoResultPayload.PlayerProfile.DisplayName;
+                if (!string.IsNullOrEmpty(displayName))
+                {
+                    PlayerPrefs.SetString("Username", displayName);
+                }
+                else
+                {
+                    // Handle the case where the display name is null or empty
+                    PlayerPrefs.SetString("Username", "DefaultUsername");
+                }
+
+                OnLoginSuccess();
+            }
+            else
+            {
+                // Handle the case where the result or its properties are null
+                messageText.text = "Error: Unable to retrieve player profile information.";
+                Debug.LogError("LoginUser - Null reference encountered in result or its properties.");
+            }
+        },
+        error => PlayFabFailure(error));
+    }
+    void RegisterUser(string email, string username, string password)
     {
         if (passwordInput.text.Length < 6)
         {
             messageText.text = "Password too short!";
             return;
         }
-        var request = new RegisterPlayFabUserRequest
+        PlayFabClientAPI.RegisterPlayFabUser(new RegisterPlayFabUserRequest()
         {
-            Email = emailInput.text,
-            Password = passwordInput.text,
+            Email = email,
+            Password = password,
+            DisplayName = username,
             RequireBothUsernameAndEmail = false
-        };
-        PlayFabClientAPI.RegisterPlayFabUser(request, OnRegisterSuccess, OnError);
+        },
+        result => LoginUser(email, password),
+        error => PlayFabFailure(error));
     }
 
-    void OnRegisterSuccess(RegisterPlayFabUserResult result)
+    void OnRegisterSuccess()
     {
         messageText.text = "Registered and logged in!";
         SceneManager.LoadScene("Lobby");
     }
 
-    void OnError(PlayFabError error)
+    void PlayFabFailure(PlayFabError error)
     {
         messageText.text = "Error: " + error.ErrorMessage;
         Debug.Log("PlayFab Error: " + error.GenerateErrorReport());
     }
-    void OnLoginSuccess(LoginResult result)
+
+    void OnLoginSuccess()
     {
         messageText.text = "Logged in successfully!";
         SceneManager.LoadScene("Lobby");
     }
-
 }
