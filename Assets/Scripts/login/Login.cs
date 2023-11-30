8aad800918fc23fb0f1af7b6e669369c5bb50e72
DevEpi <blockproductions123@gmail.com>
2023-11-30 02:21:39 -0800

    overhauled login
    new login system

--- Assets/Scripts/login/Login.cs
+++ Assets/Scripts/login/Login.cs
@@ -1,14 +1,18 @@
-using TMPro;
+using System;
+using System.Collections;
+using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
-using Photon.Pun;
-using Photon.Realtime;
-using System.Collections.Generic; // Ensure this namespace is included for Dictionary usage
+using UnityEngine.SceneManagement;
 using TMPro;
+using PlayFab;
+using PlayFab.ClientModels;
 
-public class Login : MonoBehaviourPunCallbacks
+public class Login : MonoBehaviour
 {
-    public TMP_InputField usernameInput;
+    [Header("UI")]
+    public TMP_Text messageText;
+    public TMP_InputField emailInput;
     public TMP_InputField passwordInput;
     public Button loginButton;
     public Button registerButton;
@@ -21,47 +25,46 @@ public class Login : MonoBehaviourPunCallbacks
 
     void LoginUser()
     {
-        if (!string.IsNullOrEmpty(usernameInput.text) && !string.IsNullOrEmpty(passwordInput.text))
+        var request = new LoginWithEmailAddressRequest
         {
-            // Connect to Photon's server
-            PhotonNetwork.ConnectUsingSettings();
-        }
-        else
-        {
-            Debug.Log("Username or password is empty.");
-        }
+            Email = emailInput.text,
+            Password = passwordInput.text
+        };
+        PlayFabClientAPI.LoginWithEmailAddress(request, OnLoginSuccess, OnError);
     }
 
-    public override void OnConnectedToMaster()
-    {
-        Debug.Log("Connected to Photon!");
-
-        // Authenticate with Photon's Cloud using username as a player's NickName
-        PhotonNetwork.AuthValues = new AuthenticationValues(usernameInput.text);
-        PhotonNetwork.NickName = usernameInput.text; // Setting username as NickName for Photon
-
-        // Join the default lobby (or a specific lobby)
-        PhotonNetwork.JoinLobby();
-    }
-
-    public override void OnJoinedLobby()
-    {
-        Debug.Log("Joined lobby!");
-
-        // You can perform additional actions here after joining the lobby, such as loading another scene
-        GoToLobby();
-    }
-
-    void GoToLobby()
-    {
-        // Load the lobby scene after successful login
-        // Replace "LobbySceneName" with your actual lobby scene name
-        UnityEngine.SceneManagement.SceneManager.LoadScene("LobbySceneName");
-    }
-
+  
     void RegisterUser()
     {
-        // Handle user registration on your server-side logic, not within PUN
-        Debug.Log("Account registration should be handled on the server.");
+        if (passwordInput.text.Length < 6)
+        {
+            messageText.text = "Password too short!";
+            return;
+        }
+        var request = new RegisterPlayFabUserRequest
+        {
+            Email = emailInput.text,
+            Password = passwordInput.text,
+            RequireBothUsernameAndEmail = false
+        };
+        PlayFabClientAPI.RegisterPlayFabUser(request, OnRegisterSuccess, OnError);
     }
+
+    void OnRegisterSuccess(RegisterPlayFabUserResult result)
+    {
+        messageText.text = "Registered and logged in!";
+        SceneManager.LoadScene("Lobby");
+    }
+
+    void OnError(PlayFabError error)
+    {
+        messageText.text = "Error: " + error.ErrorMessage;
+        Debug.Log("PlayFab Error: " + error.GenerateErrorReport());
+    }
+    void OnLoginSuccess(LoginResult result)
+    {
+        messageText.text = "Logged in successfully!";
+        SceneManager.LoadScene("Lobby");
+    }
+
 }
