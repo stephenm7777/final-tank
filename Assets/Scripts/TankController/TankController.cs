c992758442d50a7ac125c4cc0eae449a679430cc
Stephen Martinez <122754065+stephenm7777@users.noreply.github.com>
2023-12-04 22:10:47 -0800

    updating game

---
+++ Assets/Scripts/TankController/TankController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tank { 
+    [RequireComponent(typeof(Rigidbody))]
+    [RequireComponent(typeof(TankInput))]
+    public class TankController : MonoBehaviour{
+        #region Variables 
+        [Header("Movement Properties")]
+        private Rigidbody rb;
+        private TankInput input; 
+        public float tankSpeed = 30f; 
+        public float tankRotationSpeed = 30f; 
+        #endregion
+
+        #region BuiltInMethods
+        void Start(){
+            rb = GetComponent<Rigidbody>();
+            input = GetComponent<TankInput>();
+        }
+        
+        void FixedUpdate(){
+            if(rb && input){
+                HandleMovement();
+            }
+        }
+        #endregion
+
+        #region Custom Methods 
+        protected virtual void HandleMovement(){
+            Vector3 wantedPosition = transform.position + (transform.forward * input.FowardInput * tankSpeed * Time.deltaTime);
+            rb.MovePosition(wantedPosition);
+
+            //Rotate the Tank 
+            Quaternion wantedRotation = transform.rotation * Quaternion.Euler(Vector3.up * (tankRotationSpeed * input.RotationInput * Time.deltaTime));
+            rb.MoveRotation(wantedRotation);
+        }
+        #endregion
+        
+    }
+}
